86d410fef58f788584a204b166e722fcc73ef0c2
juanjo <jponzvan@mercadona.es>
2020-02-27 22:03:30 +0100

    Local team can score two points

--- scoreboard/scoreboard.py
+++ scoreboard/scoreboard.py
@@ -3,7 +3,10 @@ class ScoreBoard:
         self.local = 0
 
     def add_one_for_local(self):
-        self.local +=1
+        self.local += 1
+
+    def add_two_for_local(self):
+        self.local += 2
 
     def result(self):
         return f'00{self.local}-000'
--- tests/test_scoreboard.py
+++ tests/test_scoreboard.py
@@ -3,15 +3,31 @@ from scoreboard.scoreboard import ScoreBoard
 
 class TestScoreBoard:
 
-    def test_returns_result_when_local_score_nine_points(self):
+    def test_returns_score_when_local_score_one_points(self):
         score_board = ScoreBoard()
 
-        for point in range(9):
+        score_board.add_one_for_local()
+
+        assert score_board.result() == '001-000'
+
+    def test_returns_score_when_local_score_two_points(self):
+        score_board = ScoreBoard()
+        score_board.add_one_for_local()
+
+        score_board.add_two_for_local()
+
+        assert score_board.result() == '003-000'
+
+    def test_returns_score_when_local_score_nine_points(self):
+        score_board = ScoreBoard()
+
+        score_board.add_two_for_local()
+        for point in range(7):
             score_board.add_one_for_local()
 
         assert score_board.result() == '009-000'
 
-    def test_returns_result_when_local_score_ten_points(self):
+    def test_returns_score_when_local_score_ten_points(self):
         score_board = ScoreBoard()
 
         for point in range(10):
